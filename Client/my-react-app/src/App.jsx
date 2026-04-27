387a6f8a8f7f217d51a7e7003928a96a17b66896
Disha rajpoot <rajpootdisha42@gmail.com>
2026-04-27 19:52:41 +0530

    app

--- Client/my-react-app/src/App.jsx
+++ Client/my-react-app/src/App.jsx
@@ -7,7 +7,7 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
  */
-
+ 
 import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
 import { Sidebar, Navbar } from './components/Layout';
