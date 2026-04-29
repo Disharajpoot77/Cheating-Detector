4080416dcf7503134a18a614bcd4b066871fb83c
Disha rajpoot <rajpootdisha42@gmail.com>
2026-04-29 19:32:44 +0530

    session

--- Client/my-react-app/src/screens/sessionReport.jsx
+++ Client/my-react-app/src/screens/sessionReport.jsx
@@ -163,6 +163,6 @@ const InsightCard = ({ title, score, desc, type = 'normal' }) => (
             />
         </div>
         <p className="text-[11px] text-gray-500 dark:text-slate-400 leading-relaxed">{desc}</p>
-    </div> 
+    </div>  
 );
  
